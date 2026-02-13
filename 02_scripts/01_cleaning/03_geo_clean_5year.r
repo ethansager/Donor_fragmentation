f63100a1d77b82a13b22f76ce0abb360f1dfee30
ethansager <ethansagermel@gmail.com>
2026-02-12 20:37:54 -0500

    Add 5-year bucket panel and IV build pipeline

---
+++ 02_scripts/01_cleaning/03_geo_clean_5year.r
@@ -0,0 +1,251 @@
+# Cleaning datasets script (5-year buckets)
+# Set up and packages ---------------------------------------------------------
+
+if (!require("pacman")) {
+  install.packages("pacman")
+}
+pacman::p_load(
+  tidyverse,
+  here,
+  readr,
+  janitor,
+  scales,
+  psych,
+  tidylog,
+  units,
+  DescTools,
+  zoo,
+  data.table,
+  sf,
+  geodata,
+  terra,
+  nngeo
+)
+
+# Note the below functions are sourced from utils
+source("02_scripts/utils/godad_processing_utils.r")
+
+map_to_5year_bucket <- function(year) {
+  dplyr::case_when(
+    year %in% 2005:2009 ~ 2005,
+    year %in% 2010:2015 ~ 2010,
+    TRUE ~ NA_real_
+  )
+}
+
+safe_mean <- function(x) {
+  if (all(is.na(x))) {
+    return(NA_real_)
+  }
+  mean(x, na.rm = TRUE)
+}
+
+safe_max <- function(x) {
+  if (all(is.na(x))) {
+    return(NA_real_)
+  }
+  max(x, na.rm = TRUE)
+}
+
+## Load data
+raw_dat <- read_csv(here("00_rawdata", "GODAD_projectlevel.csv"))
+
+# Get country isos for gadm
+countries_iso3 <- read_sf("00_rawdata/shapefiles/gadm_admin2.shp") %>%
+  st_drop_geometry() %>%
+  select(GID_0) %>%
+  distinct() %>%
+  pull()
+
+# Round 3 2005-2006
+# Round 4 2008-2009
+# Round 5 2011-2013
+# Round 6 2014-2015
+
+# Clean CRS data as it does not have IATI codes
+raw_dat <- raw_dat %>%
+  mutate(
+    sector_main = as.numeric(str_split_fixed(sector_codes, "\\|", 2)[, 1]),
+    early_impact = case_when(
+      sector_main %in% 110:160 ~ 0,
+      sector_main %in% 210:230 ~ 1,
+      sector_main %in% 231 ~ 0,
+      sector_main %in% 232:323 ~ 1,
+      sector_main %in% 331:998 ~ 0,
+      TRUE ~ NA
+    ),
+    bucket_5year = map_to_5year_bucket(paymentyear),
+    world_bank = ifelse(
+      donor == "World Bank" & !precision_code %in% c("1", "2", "3"),
+      1,
+      0
+    )
+  ) %>%
+  filter(
+    gid_0 %in% countries_iso3,
+    paymentyear >= 2005,
+    paymentyear <= 2015,
+    !is.na(latitude),
+    !is.na(longitude),
+    !is.na(bucket_5year),
+    world_bank == 0
+  ) %>%
+  mutate(paymentyear = bucket_5year)
+
+# Attach study_group using mapping based on sector_main
+sector_info <- apply_sector_mapping(raw_dat)
+raw_dat <- sector_info$dat
+aid_type_codes <- sector_info$aid_type_codes
+
+shp <- read_sf("00_rawdata/shapefiles/gadm_admin2.shp") %>%
+  st_make_valid() %>%
+  st_transform(4326)
+
+# Convert aid dataset to an sf object
+aid_points <- st_as_sf(
+  raw_dat,
+  coords = c("longitude", "latitude"),
+  crs = 4326
+)
+
+# Perform a spatial join to link aid points to admin-2 polygons
+dat <- st_join(aid_points, st_make_valid(shp))
+
+# Convert dat to data.table for speed
+setDT(dat)
+
+# Population grid GWP (bucketed)
+admin1_pop <- read_csv("00_rawdata/population/admin1_population.csv") %>%
+  pivot_longer(
+    cols = starts_with("sum_"),
+    names_to = "year",
+    values_to = "pop"
+  ) %>%
+  select(GID_1, year, pop) %>%
+  mutate(
+    year = as.numeric(gsub("sum_", "", year)),
+    year = map_to_5year_bucket(year)
+  ) %>%
+  filter(!is.na(year)) %>%
+  group_by(GID_1, year) %>%
+  summarise(pop = safe_mean(pop), .groups = "drop") %>%
+  mutate(ln_pop = log(pop))
+
+admin2_pop <- read_csv("00_rawdata/population/admin2_population.csv") %>%
+  pivot_longer(
+    cols = starts_with("sum_"),
+    names_to = "year",
+    values_to = "pop"
+  ) %>%
+  select(GID_2, year, pop) %>%
+  mutate(
+    year = as.numeric(gsub("sum_", "", year)),
+    year = map_to_5year_bucket(year)
+  ) %>%
+  filter(!is.na(year)) %>%
+  group_by(GID_2, year) %>%
+  summarise(pop = safe_mean(pop), .groups = "drop") %>%
+  mutate(ln_pop = log(pop))
+
+###### Read in Afro data and create panel to match ########
+
+admin1_afro <- read_csv("00_rawdata/ab_raw/processed/admin1_afro_panel.csv") %>%
+  select(year, GID_0, GID_1, starts_with("mean_"), afro_count, wave) %>%
+  mutate(year = map_to_5year_bucket(year)) %>%
+  filter(!is.na(year)) %>%
+  group_by(year, GID_0, GID_1) %>%
+  summarise(
+    across(starts_with("mean_"), ~ safe_mean(.x)),
+    afro_count = safe_mean(afro_count),
+    wave = safe_max(wave),
+    .groups = "drop"
+  )
+
+admin2_afro <- read_csv("00_rawdata/ab_raw/processed/admin2_afro_panel.csv") %>%
+  select(year, GID_0, GID_2, starts_with("mean_"), afro_count, wave) %>%
+  mutate(year = map_to_5year_bucket(year)) %>%
+  filter(!is.na(year)) %>%
+  group_by(year, GID_0, GID_2) %>%
+  summarise(
+    across(starts_with("mean_"), ~ safe_mean(.x)),
+    afro_count = safe_mean(afro_count),
+    wave = safe_max(wave),
+    .groups = "drop"
+  )
+
+##### Read in dependent variables (bucketed) #####
+
+admin1_dep_vars <- read_csv("00_rawdata/processed_dep_vars_admin1.csv") %>%
+  mutate(year = map_to_5year_bucket(year)) %>%
+  filter(!is.na(year)) %>%
+  group_by(GID_0, GID_1, year) %>%
+  summarise(
+    sum = safe_mean(sum),
+    mean = safe_mean(mean),
+    u5m = safe_mean(u5m),
+    .groups = "drop"
+  )
+
+admin2_dep_vars <- read_csv("00_rawdata/processed_dep_vars_admin2.csv") %>%
+  mutate(year = map_to_5year_bucket(year)) %>%
+  filter(!is.na(year)) %>%
+  group_by(GID_0, GID_1, GID_2, year) %>%
+  summarise(
+    sum = safe_mean(sum),
+    mean = safe_mean(mean),
+    u5m = safe_mean(u5m),
+    .groups = "drop"
+  )
+
+###### WGI (bucketed) ######
+wgi <- readxl::read_xlsx("00_rawdata/wgidataset.xlsx") %>%
+  select(GID_0, year, ge_pct) %>%
+  mutate(ge_pct = suppressWarnings(as.numeric(ge_pct))) %>%
+  mutate(year = map_to_5year_bucket(year)) %>%
+  filter(!is.na(year)) %>%
+  group_by(GID_0, year) %>%
+  summarise(ge_pct = safe_mean(ge_pct), .groups = "drop")
+
+#------------------------------------------------------------------------------#
+# Run data for all panels
+#------------------------------------------------------------------------------#
+
+build_panels_for_type(dat, "all", "_5year")
+
+sector_admin1_list <- list()
+sector_admin2_list <- list()
+
+for (type_name in names(aid_type_codes)) {
+  dat_type <- dat[study_group == type_name]
+  sector_result <- build_panels_for_type(
+    dat_type,
+    type_name,
+    paste0("_", type_name, "_5year")
+  )
+
+  sector_result$panel_aid_admin1_fin$study_group <- type_name
+  sector_result$panel_aid_admin2_fin$study_group <- type_name
+
+  sector_admin1_list[[type_name]] <- sector_result$panel_aid_admin1_fin
+  sector_admin2_list[[type_name]] <- sector_result$panel_aid_admin2_fin
+}
+
+#------------------------------------------------------------------------------#
+# Write out final long datasets
+#------------------------------------------------------------------------------#
+
+if (length(sector_admin1_list) > 0) {
+  panel_aid_admin1_by_sector <- bind_rows(sector_admin1_list)
+  write_csv(
+    panel_aid_admin1_by_sector,
+    "01_panel_data/panel_aid_admin1_by_sector_5year.csv"
+  )
+}
+
+if (length(sector_admin2_list) > 0) {
+  panel_aid_admin2_by_sector <- bind_rows(sector_admin2_list)
+  write_csv(
+    panel_aid_admin2_by_sector,
+    "01_panel_data/panel_aid_admin2_by_sector_5year.csv"
+  )
+}
---
+++ 02_scripts/01_cleaning/04_create_iv_5year.r
@@ -0,0 +1,217 @@
+# Create IV for 5-year bucketed panels
+
+if (!require("pacman")) {
+  install.packages("pacman")
+}
+
+pacman::p_load(
+  tidyverse,
+  data.table,
+  tidyr,
+  here,
+  sf,
+  zoo
+)
+
+map_to_5year_bucket <- function(year) {
+  dplyr::case_when(
+    year %in% 2005:2009 ~ 2005,
+    year %in% 2010:2015 ~ 2010,
+    TRUE ~ NA_real_
+  )
+}
+
+# Get country isos for gadm
+countries_iso3 <- read_csv(
+  "00_rawdata/nightlights/topcodefix/processed_topcodefix_nl_admin2.csv"
+) %>%
+  select(GID_0) %>%
+  distinct() %>%
+  pull()
+
+# Read in data
+raw_aid_data <- read_csv(here("00_rawdata", "GODAD_projectlevel.csv")) %>%
+  mutate(
+    world_bank = ifelse(
+      donor == "World Bank" & !precision_code %in% c("1", "2", "3"),
+      1,
+      0
+    ),
+    year = map_to_5year_bucket(paymentyear)
+  ) %>%
+  filter(
+    gid_0 %in% countries_iso3,
+    paymentyear >= 2005,
+    paymentyear <= 2015,
+    !is.na(latitude),
+    !is.na(longitude),
+    !is.na(year),
+    world_bank == 0
+  )
+
+donor_countries <- c(
+  "Austria",
+  "Belgium",
+  "Denmark",
+  "Finland",
+  "France",
+  "Germany",
+  "Greece",
+  "Iceland",
+  "India",
+  "Ireland",
+  "Italy",
+  "Luxembourg",
+  "Netherlands",
+  "Norway",
+  "Portugal",
+  "Spain",
+  "Sweden",
+  "Switzerland",
+  "United Kingdom",
+  "United States",
+  "World Bank"
+)
+
+# Combine all shapefiles into one
+shp <- sf::read_sf("00_rawdata/shapefiles/gadm_admin2.shp") %>%
+  mutate(GID_2 = ifelse(is.na(GID_2), GID_1, GID_2))
+
+aid_points <- sf::st_as_sf(
+  raw_aid_data,
+  coords = c("longitude", "latitude"),
+  crs = 4326
+)
+
+dat <- sf::st_join(aid_points, sf::st_make_valid(shp)) %>%
+  filter(donor %in% donor_countries)
+
+setDT(dat)
+
+aid_data <- dat[,
+  .(
+    aid_amount = sum(abs(disb_loc_evensplit), na.rm = TRUE)
+  ),
+  by = .(GID_0, GID_1, GID_2, year, donor)
+]
+
+# Read in annual donor fractionalization data
+frac_data_annual <- read_csv("00_rawdata/DPI2020.csv") %>%
+  mutate(
+    countryname = str_replace(countryname, "USA", "United States"),
+    countryname = str_replace(countryname, "UK", "United Kingdom"),
+    countryname = str_replace(countryname, "FRG/Germany", "Germany"),
+    frac_full = if_else(
+      countryname == "United States",
+      frac,
+      govfrac
+    )
+  ) %>%
+  filter(countryname %in% donor_countries & year >= 2005 & year <= 2015) %>%
+  select(donor = countryname, year, frac_full)
+
+# Estimate weighted average donor fractionalization for World Bank
+votes <- read_csv("00_rawdata/pdfs/wb_vote_shares.csv") %>%
+  mutate(
+    Country = str_replace(Country, "USA", "United States"),
+    Country = str_replace(Country, "UK", "United Kingdom")
+  ) %>%
+  pivot_longer(
+    cols = starts_with("Vote_"),
+    names_to = "year",
+    values_to = "vote_share"
+  ) %>%
+  mutate(year = str_replace(year, "Vote_", "")) %>%
+  filter(year != "avg") %>%
+  mutate(year = as.numeric(year)) %>%
+  complete(Country = unique(Country), year = 2005:2015) %>%
+  group_by(Country) %>%
+  mutate(vote_share = na.approx(vote_share, na.rm = TRUE, rule = 2)) %>%
+  ungroup() %>%
+  left_join(
+    frac_data_annual %>% select(donor, year, frac_full),
+    by = c("Country" = "donor", "year")
+  ) %>%
+  group_by(year) %>%
+  summarise(
+    frac_full = weighted.mean(frac_full, vote_share, na.rm = TRUE),
+    .groups = "drop"
+  ) %>%
+  mutate(donor = "World Bank")
+
+frac_data <- bind_rows(frac_data_annual, votes) %>%
+  mutate(year = map_to_5year_bucket(year)) %>%
+  filter(!is.na(year)) %>%
+  group_by(donor, year) %>%
+  summarise(frac_full = mean(frac_full, na.rm = TRUE), .groups = "drop")
+
+construct_shift_share_iv_bucketed <- function(
+    aid_data,
+    frac_data,
+    panel_data,
+    region_id,
+    aid_threshold = 0
+) {
+  periods <- sort(unique(panel_data$year))
+
+  pji <- aid_data %>%
+    complete(
+      !!sym(region_id),
+      donor,
+      year = periods,
+      fill = list(aid_amount = 0)
+    ) %>%
+    mutate(received_aid = ifelse(aid_amount > aid_threshold, 1, 0)) %>%
+    group_by(!!sym(region_id), donor) %>%
+    summarise(pji = mean(received_aid, na.rm = TRUE), .groups = "drop")
+
+  names(pji)[names(pji) == region_id] <- "region_id"
+
+  iv_components <- panel_data %>%
+    select(region_id = !!sym(region_id), year) %>%
+    distinct() %>%
+    crossing(donor = unique(frac_data$donor)) %>%
+    left_join(pji, by = c("region_id", "donor")) %>%
+    left_join(frac_data, by = c("donor", "year")) %>%
+    mutate(iv_component = pji * frac_full)
+
+  iv_data <- iv_components %>%
+    group_by(region_id, year) %>%
+    summarise(IV = sum(iv_component, na.rm = TRUE), .groups = "drop") %>%
+    arrange(region_id, year) %>%
+    group_by(region_id) %>%
+    mutate(IV_lag = dplyr::lag(IV, order_by = year)) %>%
+    ungroup()
+
+  return(iv_data)
+}
+
+# Create the IV (admin2)
+panel_aid_admin2 <- read_csv(here("01_panel_data", "panel_aid_admin2_5year.csv"))
+
+results <- construct_shift_share_iv_bucketed(
+  aid_data = aid_data,
+  frac_data = frac_data,
+  panel_data = panel_aid_admin2,
+  region_id = "GID_2"
+)
+
+panel_aid_admin2 <- panel_aid_admin2 %>%
+  left_join(results, by = c("GID_2" = "region_id", "year"))
+
+# Create the IV (admin1)
+panel_aid_admin1 <- read_csv(here("01_panel_data", "panel_aid_admin1_5year.csv"))
+
+results <- construct_shift_share_iv_bucketed(
+  aid_data = aid_data,
+  frac_data = frac_data,
+  panel_data = panel_aid_admin1,
+  region_id = "GID_1"
+)
+
+panel_aid_admin1 <- panel_aid_admin1 %>%
+  left_join(results, by = c("GID_1" = "region_id", "year"))
+
+# Save the data
+write_csv(panel_aid_admin2, here("01_panel_data", "panel_aid_admin2_fin_5year.csv"))
+write_csv(panel_aid_admin1, here("01_panel_data", "panel_aid_admin1_fin_5year.csv"))
